f8692fa265b468dddf3d3df539f459aed28030d0
Roman Gurevitch <roman.gurevitch@digio.com.au>
2026-03-19 16:50:15 +1100

    fix: exclude student challenges from default test run to pass CI

--- test.mk
+++ test.mk
@@ -2,20 +2,20 @@
 
 .PHONY: test test-hello test-basics test-bank test-challenges bench
 
-test: generate ## Run all tests
-	go test ./...
+test: generate ## Run all tests (excluding student challenges)
+	go test $$(go list ./... | grep -v "/internal/basics/challenges")
 
 test-hello: ## Run all hello world tests
 	go test ./cmd/hello/... ./internal/hello/...
 
-test-basics: ## Run module 2 (Go basics) tests
-	go test ./internal/basics/...
+test-basics: ## Run module 2 (Go basics) tests (excluding challenges)
+	go test $$(go list ./internal/basics/... | grep -v "/internal/basics/challenges")
 
 test-bank: generate ## Run module 3 (Go Bank) tests
 	go test ./internal/bank/...
 
-test-challenges: ## Run all challenge tests
-	go test ./internal/challenges/...
+test-challenges: ## Run all basics challenge tests
+	go test ./internal/basics/challenges/...
 
 bench: ## Run all benchmarks
 	go test -bench=. -benchmem ./...
